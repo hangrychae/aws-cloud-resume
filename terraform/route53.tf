a42c8fc4b402bf8a1f6201f1c65505a9e2da3a97
Chae <117418774+hangrychae@users.noreply.github.com>
2023-03-17 11:05:40 -0400

    Update route53.tf

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -15,17 +15,6 @@ resource "aws_route53_record" "root-a" {
   }
 }
 
-# resource "aws_route53_record" "www-a" {
-#   zone_id = aws_route53_zone.main.zone_id
-#   name = "www.${var.domain_name}"
-#   type = "A"
-
-#   alias {
-#     name = aws_cloudfront_distribution.s3_distribution.domain_name
-#     zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
-#     evaluate_target_health = false
-#   }
-# }
 
 resource "aws_route53_record" "cert_validation" {
   for_each = {
