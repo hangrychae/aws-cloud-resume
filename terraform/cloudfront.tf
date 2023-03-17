9536da3920579a3d7eb0d97acda1d8efec501898
Chae <117418774+hangrychae@users.noreply.github.com>
2023-03-17 11:06:36 -0400

    Update cloudfront.tf

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -28,7 +28,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   default_cache_behavior {
     allowed_methods = ["GET", "HEAD"]
     cached_methods = ["GET", "HEAD"]
-    # target_origin_id = "S3-www.${var.bucket_name}"
 	target_origin_id = "S3-${var.bucket_name}"
 
     forwarded_values {
@@ -61,56 +60,4 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   tags = var.common_tags
 }
 
-# Cloudfront S3 for redirect to www.
-# resource "aws_cloudfront_distribution" "root_s3_distribution" {
-#   origin {
-#     domain_name = aws_s3_bucket.root_bucket.website_endpoint
-#     origin_id = "S3-${var.bucket_name}"
-#     custom_origin_config {
-#       http_port = 80
-#       https_port = 443
-#       origin_protocol_policy = "http-only"
-#       origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-#     }
-#   }
 
-#   enabled = true
-#   is_ipv6_enabled = true
-
-#   aliases = [var.domain_name]
-
-#   default_cache_behavior {
-#     allowed_methods = ["GET", "HEAD"]
-#     cached_methods = ["GET", "HEAD"]
-#     target_origin_id = "S3-.${var.bucket_name}"
-
-#     forwarded_values {
-#       query_string = true
-
-#       cookies {
-#         forward = "none"
-#       }
-
-#       headers = ["Origin"]
-#     }
-
-#     viewer_protocol_policy = "allow-all"
-#     min_ttl = 0
-#     default_ttl = 10
-#     max_ttl = 10
-#   }
-
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#     }
-#   }
-
-#   viewer_certificate {
-#     acm_certificate_arn      = aws_acm_certificate_validation.cert_validation.certificate_arn
-#     ssl_support_method = "sni-only"
-#     minimum_protocol_version = "TLSv1.2_2021"
-#   }
-
-#   tags = var.common_tags
-# }
