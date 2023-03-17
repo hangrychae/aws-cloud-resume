0d8045d127911a5946595ca7b62fd68eb77ba7e2
Chae <117418774+hangrychae@users.noreply.github.com>
2023-03-17 11:04:55 -0400

    Update pytest.py

--- test/pytest.py
+++ test/pytest.py
@@ -9,7 +9,6 @@ class TestMyWebsite(unittest.TestCase):
     def test_about_page_links(self):
         response = requests.get('https://chaecloudresume.com/#about.html')
         self.assertEqual(response.status_code, 200)
-        # self.assertIn('Contact', response.content.decode())
 	
 
 if __name__ == '__main__':
